11724b657c785cc3e6d26f3de04424a378e7792b
Kenan <kenan.fabre.dwwm48@gmail.com>
2025-01-22 14:11:15 +0100

    restructuration
    restructuration de node, tailwind car j'ai  pc reset donc le javascript ne marchais plus.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './styles/**/*.css'],
+  content: [
+    './index.html',
+    './styles/**/*.css',
+    './styles/**/*.html', // Si vous avez des fichiers HTML dans ce dossier aussi
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
